89c6dd96cedbfffe162cf4890e69398483a26e30
vkn_yoga <filippo.valmori@gmail.com>
2025-10-06 09:42:55 +0200

    182

--- 4-INF/Python/music_launcher.py
+++ 4-INF/Python/music_launcher.py
@@ -3,27 +3,48 @@
 
 from datetime import datetime
 from glob import glob
-from os import path, remove, startfile
+from os import path, remove
+from platform import system
 from random import randrange, seed
 from re import search, IGNORECASE
+from subprocess import call
 from sys import argv
+from time import sleep
+
 
 
 ### PARAMETERS ###
 
-path_to_songs = 'C:\\Users\\VKN\\Music\\'                 # absolute or relative path to songs folder
-songs_extens = '.mp3'                                     # songs extension to be filtered
+path_to_songs_win = 'C:\\Users\\VKN\\Music\\'                   # absolute or relative path to songs folder for Windows OS
+path_to_songs_lnx = '/home/vkn/Music/'                          # absolute or relative path to songs folder for Linux OS
+songs_extens = '.mp3'                                           # songs extension to be filtered
+
 
 
 ### CONSTANTS ###
 
-size_type = "--size"                                      # type id for random fixed-sized playlist
-pattern_type = "--pattern"                                # type id for string pattern playlist
-playlist_name = 'vkn_playlist.m3u'                        # name of temporary .m3u playlist
+size_type = "--size"                                            # type id for random fixed-sized playlist
+pattern_type = "--pattern"                                      # type id for string pattern playlist
+playlist_name = 'vkn_playlist.m3u'                              # name of temporary .m3u playlist
+expected_os = 'Linux'                                           # expected OS
+remove_delay = 0.2                                              # delay between playlist launch an removal
+
 
 
 ### PROCESSING ###
 
+# set variables and load additional libraries according to detected OS
+detected_os = system()
+print(detected_os)
+if detected_os == 'Linux' :
+  path_to_songs = path_to_songs_lnx
+  from subprocess import call
+elif detected_os == 'Windows' :
+  path_to_songs = path_to_songs_win
+  from os import startfile
+else :
+  err_msg = 'Invalid OS detected: ' + detected_os
+  raise Exception(err_msg)
 # retrieve playlist type from 1st terminal argument
 if len(argv)-1 < 2:
   error_msg = "Invalid number of input arguments provided"
@@ -69,15 +90,22 @@ with open(playlist_name, 'w') as fid:
   fid.write(playlist_text)
 # print playlist recap on terminal 
 print(playlist_recap)
-# start playlist with predefined app (e.g. Windows Media Player)
-startfile(playlist_name)
+# start playlist with predefined app (e.g. MediaPlayer or Celluloid)
+if detected_os == 'Windows' :
+  startfile(playlist_name)
+else :
+  call(['open', playlist_name])
 # delete playlist after launching
 if path.exists(playlist_name) :
+  sleep(remove_delay)
   remove(playlist_name)
+
 
 
 ### NOTES ###
 
 # 1. How to use this script:
-#    - to create and launch a random playlist of specified size >> "cls && python playlist_launcher.py --size SIZE" (e.g. SIZE = 20);
-#    - to create and lauch a playlist with all songs whose title matches a specified patter >> "cls && python playlist_launcher.py --pattern STRING" (e.g. STRING = red hot).
+#    - to create and launch a random playlist of specified size >>
+#       "python playlist_launcher.py --size SIZE" (e.g. SIZE = 20);
+#    - to create and lauch a playlist with all songs whose title matches a specified patter >>
+#       "python playlist_launcher.py --pattern STRING" (e.g. STRING = red hot).
